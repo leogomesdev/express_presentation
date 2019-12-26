26d9ef02932bd9f12a43473266fb9c8504562ff2
Leonardo Gomes <leogomesdev@gmail.com>
2019-12-26 00:08:44 -0300

    07 rejecting if user already exists

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,7 +5,11 @@ const User = require('../models/user');
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
+    const { email } = req.body;
     try {
+        if (await User.findOne({ email })) { // change to https://github.com/blakehaswell/mongoose-unique-validator
+            return res.status(400).send({ error: "Email already in yse" });
+        }
         const user = await User.create(req.body);
         user.password = undefined;
         return res.send({ user });
