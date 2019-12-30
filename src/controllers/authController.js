08d26395cc5c613c528f867c17452567045078f2
Leonardo Gomes <leogomesdev@gmail.com>
2019-12-30 17:09:29 -0300

    Correct a typo

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -8,7 +8,7 @@ router.post('/register', async (req, res) => {
     const { email } = req.body;
     try {
         if (await User.findOne({ email })) { // change to https://github.com/blakehaswell/mongoose-unique-validator
-            return res.status(400).send({ error: "Email already in yse" });
+            return res.status(400).send({ error: "Email already in use" });
         }
         const user = await User.create(req.body);
         user.password = undefined;
